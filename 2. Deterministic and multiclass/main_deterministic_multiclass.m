3bd3f5027789b2df7396a9088967cc2458ea69f9
Andrea Spinelli <156907650+aspinellibg@users.noreply.github.com>
2025-01-24 12:04:06 +0100

    Update main_deterministic_multiclass.m

--- 2. Deterministic and multiclass/main_deterministic_multiclass.m
+++ 2. Deterministic and multiclass/main_deterministic_multiclass.m
@@ -5,7 +5,7 @@
 % % Francesca Maggioni and Andrea Spinelli (University of Bergamo, IT)
 
 % % Reference:
-% % Preprint available at https://arxiv.org/abs/2306.06223
+% % F. Maggioni, A. Spinelli. (2025). _A Novel Robust Optimization Model for Nonlinear Support Vector Machine_. European Journal of Operational Research, 322(1), 237-253. https://doi.org/10.1016/j.ejor.2024.12.014
 
 
 % % Case: deterministic and multiclass classifier
